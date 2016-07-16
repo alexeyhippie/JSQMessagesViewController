dd9241809027317f8d73404c1bac42d038ae2739
Alexey Hippie <alexey.hippie@gmail.com>
2016-07-16 19:54:44 +0300

    These additinal insets also counted in;

--- JSQMessagesViewController/Controllers/JSQMessagesViewController.m
+++ JSQMessagesViewController/Controllers/JSQMessagesViewController.m
@@ -462,7 +462,7 @@ JSQMessagesKeyboardControllerDelegate>
         //  when the collection view content size is too small, `scrollToItemAtIndexPath:` doesn't work properly
         //  this seems to be a UIKit bug, see #256 on GitHub
         
-        CGFloat contentWindow = collectionViewHeight - self.collectionView.contentInset.top - self.collectionView.contentInset.bottom - self.topContentAdditionalInset - self.bottomContentAdditionalInset;
+        CGFloat contentWindow = collectionViewHeight - self.collectionView.contentInset.top - self.collectionView.contentInset.bottom;
         CGFloat offsetH = MAX(0.f, collectionViewContentHeight - contentWindow) - self.collectionView.contentInset.top;
         [self.collectionView setContentOffset:CGPointMake(0.f, offsetH) animated:YES];
         
